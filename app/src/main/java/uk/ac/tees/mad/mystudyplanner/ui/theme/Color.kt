614a666f414fbb3176aad02d0429c349f3100726
Shaik Abeedh <mystudyplannerproj@gmail.com>
2026-03-02 12:00:00 +0530

    Initial project setup

---
+++ app/src/main/java/uk/ac/tees/mad/mystudyplanner/ui/theme/Color.kt
@@ -0,0 +1,21 @@
+package uk.ac.tees.mad.mystudyplanner.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val BluePrimary = Color(0xFF4A90E2)
+val BlueSecondary = Color(0xFFB3D4FC)
+val BlueTertiary = Color(0xFF7FB3FF)
+
+val BlueBackground = Color(0xFFF5F9FF)
+val BlueSurface = Color(0xFFFFFFFF)
+val BlueOnPrimary = Color(0xFFFFFFFF)
+val BlueOnBackground = Color(0xFF1A1C1E)
+
+val DarkBluePrimary = Color(0xFF9CC3FF)
+val DarkBlueSecondary = Color(0xFF4A6FA5)
+val DarkBlueTertiary = Color(0xFF6EA8FF)
+
+val DarkBlueBackground = Color(0xFF0F172A)
+val DarkBlueSurface = Color(0xFF111827)
+val DarkBlueOnPrimary = Color(0xFF0F172A)
+val DarkBlueOnBackground = Color(0xFFE3E8F0)
